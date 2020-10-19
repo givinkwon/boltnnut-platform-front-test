5586e54ced2b97d7cdd58200add85356a1453675
givinkwon <rlqls505@naver.com>
2020-10-19 13:44:46 +0900

    20201019

--- containers/Home/Magazine.js
+++ containers/Home/Magazine.js
@@ -193,6 +193,7 @@ class MagazineContainer extends React.Component {
           </List>
         </MagazineBox>
         <RightArrowContainer>
+        {/*edit 10/19 */}
         {(this.state.idx*2 + 4) < magazineCount ? <RightArrow src={right} onClick = {this.sliderNext}/> : <RightArrow/>}
         </RightArrowContainer>
         <RequestBox>
