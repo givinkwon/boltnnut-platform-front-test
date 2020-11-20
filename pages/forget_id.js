e2ec2da816bfc7aa026e20acb4fd4505ec58ca81
minin han <minin1995@naver.com>
2020-11-20 18:50:42 +0900

    리팩토링:페이지

--- pages/forget_id.js
+++ pages/forget_id.js
@@ -1,26 +1,22 @@
-import React from 'react'
-import Head from 'next/head'
-import styled from 'styled-components'
-import { inject, observer } from 'mobx-react'
-
-import Nav from 'components/Nav'
-import Footer from 'components/Footer'
-
-import ForgetIdConatiner from 'containers/ForgetId'
+import React from 'react';
+import Head from 'next/head';
+import Nav from '../components/Nav';
+import Footer from '../components/Footer';
+import ForgetIdConatiner from '../containers/ForgetId';
 
 class ForgetId extends React.Component {
-  render(){
+  render() {
     return (
       <div>
         <Head>
           <title>볼트앤너트</title>
         </Head>
         <Nav />
-        <ForgetIdConatiner/>
-        <Footer/>
+        <ForgetIdConatiner />
+        <Footer />
       </div>
-    )
+    );
   }
 }
 
-export default ForgetId
+export default ForgetId;
