330efe9c3cce5134dd1246f7e314151ed85953f8
minin han <minin1995@naver.com>
2020-11-24 11:21:29 +0900

    버그:naver web master 코드 변경

--- pages/_document.js
+++ pages/_document.js
@@ -104,7 +104,8 @@ export default class MyDocument extends Document {
           <script dangerouslySetInnerHTML={this.setGoogleTags()} /> */}
           {/* Google Tag Manager */}
           <script dangerouslySetInnerHTML={{__html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);})(window,document,'script','dataLayer','GTM-PWFPPZ5');`,}}></script>
-          <meta name="naver-site-verification" content="12005c1a60612f3a3510070ec18a2d7a1553fcab"/>
+          {/* Naver webmaster */}
+          <meta name="naver-site-verification" content="4354dc20bc3fb28a16e1db9800acf406fd782d76" />
           <meta charSet="utf-8" />
           {/* SEO */}
           <meta property="og:title" content="볼트앤너트" />
