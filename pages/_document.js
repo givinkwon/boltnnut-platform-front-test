5196019cce7695cc5693a8d29868200a18392e57
givinkwon <rlqls505@naver.com>
2020-12-28 23:32:10 +0900

    기능 변경
    네이버 애널리틱스 오류 수정

--- pages/_document.js
+++ pages/_document.js
@@ -132,17 +132,8 @@ export default class MyDocument extends Document {
           {/* <script type="text/javascript" src="https://code.jquery.com/jquery-1.12.4.min.js" async ></script>
           <script type="text/javascript" src="https://cdn.iamport.kr/js/iamport.payment-1.1.5.js" async ></script> */}
           {/* Naver analic */}
-          {/* <script type="text/javascript" src="//wcs.naver.net/wcslog.js" async></script>
-          <script type="text/javascript" dangerouslySetInnerHTML={{__html: `if(!wcs_add) var wcs_add = {};wcs_add["wa"] = "a888b15a2864e";if(window.wcs) {wcs_do();}`,}} async ></script> */}
-          {/* 문법 오류 부분(네이버에서복사한원본코드)*/}
-          <script type="text/javascript" src="//wcs.naver.net/wcslog.js"></script>
-          <script type="text/javascript">
-          if(!wcs_add) var wcs_add = {};
-          wcs_add["wa"] = "a888b15a2864e";
-          if(window.wcs) {
-            wcs_do();
-          }
-          </script>
+          <script type="text/javascript" src="//wcs.naver.net/wcslog.js" async></script>
+          <script type="text/javascript" dangerouslySetInnerHTML={{__html: `if(!wcs_add) var wcs_add = {};wcs_add["wa"] = "a888b15a2864e";if(window.wcs) {wcs_do();}`,}} async ></script>
         </body>
       </html>
     );
