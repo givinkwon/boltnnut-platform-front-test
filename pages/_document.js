ffd972f6761a9007f15a38fcf788732831481bef
givinkwon <rlqls505@naver.com>
2021-01-06 12:56:34 +0900

    기능 변경
    SEO 수정

--- pages/_document.js
+++ pages/_document.js
@@ -46,51 +46,51 @@ export default class MyDocument extends Document {
   //    `
   //  };
   //}
-  setChannelTalk() {
-    return {
-      __html: `
-        (function() {
-          var w = window;
-          if (w.ChannelIO) {
-            return (window.console.error || window.console.log || function(){})('ChannelIO script included twice.');
-          }
-          var d = window.document;
-          var ch = function() {
-            ch.c(arguments);
-          };
-          ch.q = [];
-          ch.c = function(args) {
-            ch.q.push(args);
-          };
-          w.ChannelIO = ch;
-          function l() {
-            if (w.ChannelIOInitialized) {
-              return;
-            }
-            w.ChannelIOInitialized = true;
-            var s = document.createElement('script');
-            s.type = 'text/javascript';
-            s.async = true;
-            s.src = 'https://cdn.channel.io/plugin/ch-plugin-web.js';
-            s.charset = 'UTF-8';
-            var x = document.getElementsByTagName('script')[0];
-            x.parentNode.insertBefore(s, x);
-          }
-          if (document.readyState === 'complete') {
-            l();
-          } else if (window.attachEvent) {
-            window.attachEvent('onload', l);
-          } else {
-            window.addEventListener('DOMContentLoaded', l, false);
-            window.addEventListener('load', l, false);
-          }
-        })();
-        ChannelIO('boot', {
-          "pluginKey": "ec3d75f3-0b9e-4b01-a746-dfed77e31339"
-        });
-      `
-    }
-  }
+  // setChannelTalk() {
+  //   return {
+  //     __html: `
+  //       (function() {
+  //         var w = window;
+  //         if (w.ChannelIO) {
+  //           return (window.console.error || window.console.log || function(){})('ChannelIO script included twice.');
+  //         }
+  //         var d = window.document;
+  //         var ch = function() {
+  //           ch.c(arguments);
+  //         };
+  //         ch.q = [];
+  //         ch.c = function(args) {
+  //           ch.q.push(args);
+  //         };
+  //         w.ChannelIO = ch;
+  //         function l() {
+  //           if (w.ChannelIOInitialized) {
+  //             return;
+  //           }
+  //           w.ChannelIOInitialized = true;
+  //           var s = document.createElement('script');
+  //           s.type = 'text/javascript';
+  //           s.async = true;
+  //           s.src = 'https://cdn.channel.io/plugin/ch-plugin-web.js';
+  //           s.charset = 'UTF-8';
+  //           var x = document.getElementsByTagName('script')[0];
+  //           x.parentNode.insertBefore(s, x);
+  //         }
+  //         if (document.readyState === 'complete') {
+  //           l();
+  //         } else if (window.attachEvent) {
+  //           window.attachEvent('onload', l);
+  //         } else {
+  //           window.addEventListener('DOMContentLoaded', l, false);
+  //           window.addEventListener('load', l, false);
+  //         }
+  //       })();
+  //       ChannelIO('boot', {
+  //         "pluginKey": "ec3d75f3-0b9e-4b01-a746-dfed77e31339"
+  //       });
+  //     `
+  //   }
+  // }
  // async componentDidMount() {
  //   TagManager.initialize(tagManagerArgs)
  // }
@@ -98,6 +98,13 @@ export default class MyDocument extends Document {
     return (
       <html>
         <Head>
+          <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+          <meta http-equiv="X-UA-Compatible" content="IE=edge">
+          {/*--PG크로스브라우징필수내용--*/}
+          <meta http-equiv="Cache-Control" content="no-cache"/>
+          <meta http-equiv="Expires" content="0"/>
+          <meta http-equiv="Pragma" content="no-cache"/>
+         
           {/* 네이버웹마스터도구 사이트구조개선 코드*/}
           <meta name="NaverBot" content="All"/>
           <meta name="NaverBot" content="index, follow"/>
