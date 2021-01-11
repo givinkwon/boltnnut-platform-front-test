a005c1a1be8c3b9df45f0b275cd586f4fb318c66
KIMMINSEOK <remigailard80>
2021-01-11 13:36:32 +0900

    기능추가 : 모바일배너0

--- components/Content.js
+++ components/Content.js
@@ -76,13 +76,38 @@ export const FontSize18 = styled.p`
     letter-spacing: 0.5px;
   }
 `
+export const FontSize17 = styled.p`
+  font-weight: ${props => props.fontWeight ? props.fontWeight : 300};
+  color: ${props => props.color ? props.color : '#000000'};
+  text-align: ${props => props.center ? 'center' : 'left'};
+  font-family: ${props => props.eng ? 'Roboto' : 'Noto Sans KR'}, sans-serif;
+  @media (min-width: 0px) and (max-width: 767.98px) {
+    font-weight: bold;
+    font-stretch: normal;
+    font-style: normal;
+    line-height: 1.29;
+    letter-spacing: normal;
+    text-align: center;
+    font-size: 17px;
+  }
+  @media (min-width: 768px) and (max-width: 1279.98px) {
+    font-size: 14px;
+    letter-spacing: 0.25px;
+  }
+  @media (min-width: 1280px) {
+    font-size: 17px;
+  }
+`
 export const FontSize16 = styled.p`
   font-weight: ${props => props.fontWeight ? props.fontWeight : 300};
   color: ${props => props.color ? props.color : '#000000'};
   text-align: ${props => props.center ? 'center' : 'left'};
   font-family: ${props => props.eng ? 'Roboto' : 'Noto Sans KR'}, sans-serif;
   @media (min-width: 0px) and (max-width: 767.98px) {
-    font-size: 32px;
+    font-size: 16px;
+    line-height: 0.94;
+    letter-spacing: -0.4px;
+
   }
   @media (min-width: 768px) and (max-width: 1279.98px) {
     font-size: 14px;
