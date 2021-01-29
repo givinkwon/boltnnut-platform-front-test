b424b00765f057020aa33f6a6df4c94fe8fcd0d2
givinkwon <rlqls505@naver.com>
2021-01-29 17:16:45 +0900

    기능추가 : 모바일 기본정보입력

--- containers/Request/Mobile/Mobileindex.js
+++ containers/Request/Mobile/Mobileindex.js
@@ -10,6 +10,7 @@ import 'react-count-animation/dist/count.min.css';
 import { inject, observer } from "mobx-react";
 
 import MobileStep1Container from "./MobileStep1";
+import MobileRequestCardContainer from "./MobileRequestCard";
 
 @inject("DetailQuestion", "Request")
 @observer
@@ -18,9 +19,10 @@ class MobileRequestContainer extends React.Component {
     const { Request } = this.props;
     return (
       <div style={{ overflow: 'hidden' }}>
-        <Background backgroundColor={"#f6f6f6"}>
+        <Background>
           <Containerv1>
-            { Request.step_index == 1 && <MobileStep1Container page={Request.step1_index} />}
+            <MobileStep1Container page={Request.step1_index} />
+            {/* { Request.step_index == 1 && <MobileStep1Container page={Request.step1_index} />} */}
             {/* { Request.step_index == 2 && <Step2Container />}
             { Request.step_index == 3 && <Step3Container />}
             { Request.step_index == 4 && <Step4Container />} */}
