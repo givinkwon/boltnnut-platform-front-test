6f0e5dd43994db5f05c1470c3a4b7ffdf9b38521
givinkwon <rlqls505@naver.com>
2021-02-23 16:30:49 +0900

    기능추가 : 솔찬형님요청사항 메인페이지이미지수정 & 매거진 푸터

--- components/Footer.js
+++ components/Footer.js
@@ -118,7 +118,6 @@ class FooterComponent extends React.Component {
               onChange={Magazine.setCurrent}
               value={Magazine.current ? Magazine.current : '게시글을 선택해주세요.'}
               placeholder='게시글을 선택해주세요'
-              onClick={(event) => console.log(this)}
               />
           </CompanyInfoWrapper>
           <CompanyInfoWrapper style={{paddingTop: 14}}>
