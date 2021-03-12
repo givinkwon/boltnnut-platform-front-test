0eabbf653569e80ca0f440c15a4582c793d28f27
Hyeonj1223 <tsd6472@gmail.com>
2021-03-12 10:12:45 +0900

    기능추가: 버튼 클릭효과 적용, 가견적받기 새 페이지 제작, Nav바 높이 및 내용 수정, 푸터 수정

--- components/Footer.js
+++ components/Footer.js
@@ -5,10 +5,14 @@ import {inject, observer} from "mobx-react";
 
 // import Container from "./Container"; //
 import * as Text from "./Text";
+import * as Content from "components/Content";
+import * as Title from "components/Title";
 import { PRIMARY, WHITE } from "static/style";
 import Containerv1 from './Containerv1';
 import SelectComponent from './Select';
 
+
+const rowline = "/static/images/components/Footer/rowline.svg";
 const facebook_mob = "/static/images/components/Footer/facebook.svg";
 const instargram_mob = "/static/images/components/Footer/instargram.svg";
 const blog_mob = "/static/images/components/Footer/blog.svg";
@@ -21,7 +25,7 @@ const post = "/static/images/components/Footer/post_big.svg";
 const dropdown = "/static/images/components/Footer/dropdown.png";
 const separator = "/static/images/components/Footer/separator.png";
 const campustown = "/static/images/components/Footer/campustown.png";
-
+const lowdirection = "static/images/components/Footer/lowdirection.svg";
 const customStyles = {
   dropdownIndicator: () => ({
     color: '#555555',
@@ -98,8 +102,9 @@ class FooterComponent extends React.Component {
       <>
       <Footer>
         <Containerv1 style={{flexDirection: 'column'}}>
+          <img src={rowline} style={{width: 16, height: 4, marginBottom: 6}}></img>
           <TelInfoWrapper>
-            TEL. 02-926-6637
+            <Font14><span>TEL.</span> 02-926-6637</Font14>
           </TelInfoWrapper>
           <CompanyInfoWrapper>
               <span> 오전 10:00 ~ 오후 07:00 </span>
@@ -122,8 +127,7 @@ class FooterComponent extends React.Component {
               options = {this.props.Magazine.magazine_list && this.props.Magazine.magazine_list}
               getOptionLabel={(option) => option.title}
               onChange={Magazine.setCurrent}
-              value={Magazine.current ? Magazine.current : '게시글을 선택해주세요.'}
-              placeholder='게시글을 선택해주세요'
+              value={Magazine.current ? Magazine.current : this.props.Magazine.magazine_list[0]}
               />
           </CompanyInfoWrapper>
           <CompanyInfoWrapper style={{paddingTop: 14}}>
@@ -142,16 +146,16 @@ class FooterComponent extends React.Component {
             </FaqTable>
             <InfoDetailContainer>
               <span>
-                <span class="title"> 회사명 </span> (주)볼트앤너트
+                <span class="title"> 회사명 </span> (주) 볼트앤너트
               </span>
               <span>
-                <span class="title"> 대표자 </span> 윤기열
+                <span class="title"> 대표자 </span> 윤기열, 신지섭
               </span>
               <span>
                 <span class="title"> 사업자등록번호 </span> 390-87-01669
               </span>
               <span>
-                <span class="title"> 사업장 소재지 </span> 서울특별시 성북구 고려대길 27길 4, 3층
+                <span class="title"> 사업장 소재지 </span> 서울특별시 성북구 고려대길 27길 3 2층
               </span>
               <span>
                 <span class="title"> 이메일 </span> boltnnut@boltnnut.com
@@ -167,55 +171,43 @@ class FooterComponent extends React.Component {
       <>
       <Footer>
         <Containerv1 style={{flexDirection: 'column'}}>
-          <TelInfoWrapper>
-            TEL. 02-926-6637
-          </TelInfoWrapper>
+        <img src={rowline} style={{width: 10, height: 2, marginBottom: 6}}></img>
+          <Font14 style={{height: 17}}><span>TEL.</span>02-926-6637</Font14>
           <CompanyInfoWrapper>
-              <span> 오전 10:00 ~ 오후 07:00 </span>
-              <span> 점심시간 12:00 ~  01:00 </span>
-              <div>
-                휴일( 토요일, 일요일, 공휴일 )
-              </div>
+              <Font12 style={{marginBottom: 2}}> 평일 오전 10:00 ~ 오후 07:00 점심시간 12:00 ~  01:00 </Font12>
+              <Font12 style={{marginTop:0, marginBottom: 12}}>휴일 (토요일, 일요일, 공휴일 )</Font12>
           </CompanyInfoWrapper>
-          <CompanyInfoWrapper style={{paddingTop: 14}}>
+          <CompanyInfoWrapper>
+            <InfoDetailContainer>
+              <Font12 style={{letterSpacing: 0.23,marginTop: 12, marginBottom: 6}}>(주)볼트앤너트 사업자 정보
+              <img src = {lowdirection} style={{height: 5, width: 10, marginLeft: 9}}></img></Font12>
+              <Font12><span>회사명</span> (주)볼트앤너트</Font12>
+              <Font12><span>대표자</span> 윤기열, 신지섭<br/></Font12>
+              <Font12><span>사업자등록번호</span> 390-87-01669<br/></Font12>
+              <Font12><span>이메일</span> boltnnut@boltnnut.com<br/></Font12>
+              <Font12><span>사업장 소재지</span> 서울특별시 성북구 고려대길 27길 3 2층</Font12>
+            </InfoDetailContainer>
             <FaqTable>
               <span class="cell" onClick={() => Router.push("/faq")}>
-                자주찾는 질문
-              </span>
-              <img src={separator} />
-              <span class="cell" onClick={() => Router.push("/term/personal")}>
-                개인정보처리방침
+                <Font12 style={{fontWeight: 500}}>자주찾는 질문</Font12>
               </span>
               <img src={separator} />
               <span class="cell" onClick={() => Router.push("/term/policy")}>
-                이용약관
+                <Font12 style={{fontWeight: 500}}>이용약관</Font12>
+              </span>
+              <img src={separator} />
+              <span class="cell" onClick={() => Router.push("/term/personal")}>
+                <Font12 style={{fontWeight: 500}}>개인정보처리방침</Font12>
               </span>
             </FaqTable>
-            <InfoDetailContainer>
-              <span>
-                <span class="title"> 회사명 </span> (주)볼트앤너트
-              </span>
-              <span>
-                <span class="title"> 대표자 </span> 윤기열
-              </span>
-              <span>
-                <span class="title"> 사업자등록번호 </span> 390-87-01669
-              </span>
-              <span>
-                <span class="title"> 사업장 소재지 </span> 서울특별시 성북구 고려대길 27길 4, 3층
-              </span>
-              <span>
-                <span class="title"> 이메일 </span> boltnnut@boltnnut.com
-              </span>
-            </InfoDetailContainer>
+
           </CompanyInfoWrapper>
           <ImageWrapper>
-                  <img src={campustown} style={{height: 20, alignSelf: 'center'}}/>
+                  <img src={campustown} style={{height: 13, alignSelf: 'center'}}/>
                   <div class="imagebox">
-                    <img src={facebook_mob} onClick={() => window.open('https://www.facebook.com/boltnnut6637/')}/>
-                    <img src={instargram_mob} onClick={() => window.open('https://www.instargram.com/boltnnut_korea')}/>
-                    <img src={blog_mob} onClick={() => window.open('https://blog.naver.com/boltnnut_korea')}/>
-                    <img src={post_mob} onClick={() => window.open('https://post.naver.com/boltnnut_korea')}/>
+                    <img src={instargram} style={{height: 24, width: 24}}onClick={() => window.open('http://www.instargram.com/boltnnut_korea')}/>
+                    <img src={blog} style={{height: 24, width: 24}}onClick={() => window.open('https://blog.naver.com/boltnnut_korea')}/>
+                    <img src={post} style={{height: 24, width: 24}}onClick={() => window.open('https://post.naver.com/boltnnut_korea')}/>
                   </div>
           </ImageWrapper>
         </Containerv1>
@@ -232,12 +224,13 @@ export default FooterComponent;
 
 const Footer = styled.div`
   background-color: white;
-  padding: 99px 0px;
+  padding: 40px 0 34px 0;
   display : inline-flex;
   align-items: center;
   flex-direction: column;
   justify-content: center;
   @media (min-width: 0px) and (max-width: 767.98px) {
+    padding: 20px 0 17px 0;
     width: 100%
   }
   @media (min-width: 768px) and (max-width: 991.98px) {
@@ -518,10 +511,17 @@ const CompanyInfoWrapper = styled.div`
       padding-left: 10px;
     }
   }
+  @media (min-width: 0px) and (max-width: 767.99px) {
+    padding-top: 2px;
+    >div{
+      margin-bottom: 0;
+    }
+  }
+  
 `
 const TelInfoWrapper = styled.div`
   width: 100%;
-  height: auto;  
+  height: 36px;  
   font-size: 24px;
   font-weight: 900;
   font-stretch: normal;
@@ -531,6 +531,13 @@ const TelInfoWrapper = styled.div`
   text-align: left;
   color: #282c36;
   margin-bottom: 20px;
+  @media (min-width: 0px) and (max-width: 767.99px) {
+    
+    align-items: center;
+    justify-content: center;
+    height: 17px;
+    margin-bottom: 2px;
+  }
 `
 const FaqTable = styled.table`
   width: auto;
@@ -551,6 +558,10 @@ const FaqTable = styled.table`
     margin-right: 10px;
     margin-left: 10px;
   }
+  @media (min-width: 0px) and (max-width: 767.99px) {
+    margin-bottom: 16px;
+  }
+
 `
 const InfoDetailContainer = styled.div`
   width: auto;
@@ -577,6 +588,12 @@ const InfoDetailContainer = styled.div`
       margin-right: 5px;
     }
   }
+  @media (min-width: 0px) and (max-width: 767.99px) {
+    margin-top: 0;
+    margin-bottom: 0 ;
+  }
+
+  
 `
 const ImageWrapper = styled.div`
   display: flex;
@@ -585,6 +602,41 @@ const ImageWrapper = styled.div`
   .imagebox {
     > img {
       margin-left: 10px;
+      @media (min-width: 0px) and (max-width: 767.99px){
+        margin-left: 24px;
+      }
     }
   }
+`
+
+const Font14 = styled(Content.FontSize14)`
+  font-family: Roboto;
+  font-size: 14px !important;
+  font-weight: bold;
+  font-stretch: normal;
+  font-style: normal;
+  line-height: 0.71;
+  letter-spacing: normal !important;
+  color: #282c36;
+  >span{
+    font-family: Roboto;
+    line-height: 1.21;
+    letter-spacing: 0.27px !important;
+  }
+`
+const Font12 = styled(Text.FontSize12)`
+justfiy-content: center;
+align-items: center;
+  font-size: 12px !important;
+  font-stretch: normal;
+  font-style: normal;
+  line-height: 1.5;
+  letter-spacing: -0.3px;
+  text-align: left;
+  color: #282c36;
+  >span{
+    font-weight: 500;
+    color: #414550;
+  }
+  margin-top: 8px;
 `
