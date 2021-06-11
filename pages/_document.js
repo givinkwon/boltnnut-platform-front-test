8864390b860c8dbf3bcff63c97612db1ea1338ed
givinkwon <rlqls505@naver.com>
2021-06-11 18:18:22 +0900

    기능 수정 : 페이지뷰 트루
    페이지뷰 트루

--- pages/_document.js
+++ pages/_document.js
@@ -42,7 +42,7 @@ export default class MyDocument extends Document {
        window.dataLayer = window.dataLayer || [];
        function gtag(){dataLayer.push(arguments);}
        gtag('js', new Date());
-       gtag('config', 'UA-162026812-1', {'send_page_view' : false } );
+       gtag('config', 'UA-162026812-1' );
      `
    };
   }
