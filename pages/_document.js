655a06d698139fb58fe674305b32c42900e6d855
givinkwon <rlqls505@naver.com>
2021-06-11 18:47:40 +0900

    기능 수정 : gtag
    gtag 연결

--- pages/_document.js
+++ pages/_document.js
@@ -176,12 +176,12 @@ export default class MyDocument extends Document {
           <script async src="https://www.googletagmanager.com/gtag/js?id=UA-162026812-1"></script>
           <script dangerouslySetInnerHTML={this.setGoogleTags()} />
           {/* Google Tag Manager */}
-          <script
+          {/* <script
             dangerouslySetInnerHTML={{
               __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);})(window,document,'script','dataLayer','GTM-PWFPPZ5');`,
             }}
             async
-          ></script>
+          ></script> */}
           {/* Iamport */}
           <script
             type="text/javascript"
