b951faff54ef910f581f51ed7c3f2a35cf66e179
psw0962 <psw0962@naver.com>
2021-07-22 11:47:20 +0900

    js-cookie 오류수정

--- containers/Manufacture/Producer/index.js
+++ containers/Manufacture/Producer/index.js
@@ -15,31 +15,30 @@ import MobileRequestDone from "./MobileRequestDone";
 import { DiagnosticCategory } from "typescript";
 
 // cookie 추가
-import Cookies from 'js-cookie';
+import Cookies from "js-cookie";
 
-@inject("Auth", "Partner", "Category","Cookie")
+@inject("Auth", "Partner", "Category", "Cookie")
 @observer
 class ProducerConatiner extends React.Component {
   async componentDidMount() {
     let partner_view_data = [];
-    const { Auth, Partner, Category, Cookie } = this.props;    
+    const { Auth, Partner, Category, Cookie } = this.props;
     Partner.init();
     Partner.newIndex = 0;
     Partner.mobileRequestIndex = 0;
     await Auth.checkLogin();
 
-    
     // Cookie 값 가지고 와서 리스트에 먼저 저장
-    partner_view_data = Cookies.get('partner_view')
+    partner_view_data = Cookies.get("partner_view");
     // list 전처리
-    partner_view_data = partner_view_data.replace('[','').replace(']','').split(',')
-
-    if(partner_view_data !== undefined && partner_view_data !== 'undefined'){
-      partner_view_data.map((data) =>
-        Cookie.add_partner_view(data)
-      )
+    if (partner_view_data) {
+      partner_view_data = partner_view_data.replace("[", "").replace("]", "").split(",");
     }
-  }  
+
+    if (partner_view_data !== undefined && partner_view_data !== "undefined") {
+      partner_view_data.map((data) => Cookie.add_partner_view(data));
+    }
+  }
 
   render() {
     const { Auth, Partner } = this.props;
@@ -48,7 +47,7 @@ class ProducerConatiner extends React.Component {
         {this.props.width &&
           (this.props.width > 767.99 ? (
             <div>
-            {/* 제조사 찾기 기본 화면 */}
+              {/* 제조사 찾기 기본 화면 */}
               {Partner.newIndex == 0 && (
                 <>
                   <BannerConatiner />
@@ -61,13 +60,11 @@ class ProducerConatiner extends React.Component {
                 </>
               )}
               {/* 제조사 찾기 상세 페이지 */}
-              {Partner.newIndex == 1 && (
-                <DetailContainer width={this.props.width} />
-              )}
+              {Partner.newIndex == 1 && <DetailContainer width={this.props.width} />}
             </div>
           ) : (
             <>
-            {/* 제조사 찾기 모바일 버전 기본 화면 */}
+              {/* 제조사 찾기 모바일 버전 기본 화면 */}
               {Partner.mobileRequestIndex == 0 && (
                 <>
                   <MobileSearchBar width={this.props.width} />
@@ -75,13 +72,9 @@ class ProducerConatiner extends React.Component {
                 </>
               )}
               {/* 제조사 찾기 모바일 버전 업체 수배 화면 */}
-              {Partner.mobileRequestIndex == 1 && (
-                <MobileRequest width={this.props.width} />
-              )}
+              {Partner.mobileRequestIndex == 1 && <MobileRequest width={this.props.width} />}
               {/* 제조사 찾기 모바일 버전 업체 수배 완료 화면 */}
-              {Partner.mobileRequestIndex == 2 && (
-                <MobileRequestDone width={this.props.width} />
-              )}
+              {Partner.mobileRequestIndex == 2 && <MobileRequestDone width={this.props.width} />}
             </>
           ))}
       </>
