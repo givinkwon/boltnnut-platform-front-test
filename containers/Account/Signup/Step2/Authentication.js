4e518a32f68750fb8f9791aec1600c61488b0852
givinkwon <rlqls505@naver.com>
2021-07-27 14:14:36 +0900

    기능 수정 : 파트너 가입하기 필터 기능 추가
    파트너 가입하기 필터 기능 추가

--- containers/Account/Signup/Step2/Authentication.js
+++ containers/Account/Signup/Step2/Authentication.js
@@ -4,39 +4,46 @@ import { inject, observer } from "mobx-react";
 
 import * as Text from "components/Text";
 
+const plusImg = "/static/images/signup/plus.svg";
 @inject("Auth", "Answer")
 @observer
 class Authentication extends React.Component {
   render() {
     return (
       <Container>
+      <Header>
         <Info>
-          <Name>회사 인증</Name>
-          <Description>
-            인증을 할 경우 업체에 대한 신뢰도와 의뢰율이 2배이상 높아집니다.
-          </Description>
+          <Name>포트폴리오</Name>
+          <Description>인증을 할 경우 업체에 대한 신뢰도와 의뢰율이 2배이상 높아집니다.</Description>
         </Info>
 
-        <Button>인증하기</Button>
-      </Container>
-    );
-  }
+        <Button>파일 업로드하기</Button>
+      </Header>
+
+      <Main>사업자등록증 업로드</Main>
+    </Container>
+  );
 }
+}
+
 
 export default Authentication;
 
+
 const Container = styled.div`
   display: flex;
+  flex-direction: column;
   justify-content: space-between;
   align-items: center;
-  padding-bottom: 20px;
-  border-bottom: 1px solid #e1e2e4;
+
   margin-top: 100px;
   margin-bottom: 120px;
 `;
+
 const Info = styled.div`
   display: flex;
 `;
+
 const Name = styled.div`
   font-size: 24px;
   line-height: 34px;
@@ -45,6 +52,7 @@ const Name = styled.div`
   font-weight: bold;
   margin-right: 12px;
 `;
+
 const Description = styled.div`
   font-size: 16px;
   line-height: 34px;
@@ -52,6 +60,7 @@ const Description = styled.div`
   color: #555963;
   font-weight: normal;
 `;
+
 const Button = styled.button`
   font-size: 18px;
   line-height: 27px;
@@ -60,4 +69,25 @@ const Button = styled.button`
   font-weight: 600;
   background-color: #ffffff;
   border: none;
+`;
+
+const Header = styled.div`
+  display: flex;
+  justify-content: space-between;
+  width: 100%;
+  padding-bottom: 20px;
+  border-bottom: 1px solid #e1e2e4;
+  margin-bottom: 20px;
+`;
+const Main = styled.div`
+  width: 100%;
+  box-shadow: 0 0 8px 0 rgba(0, 0, 0, 0.16);
+  height: 406px;
+  position: relative;
+  > img {
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+  }
 `;
--- containers/Account/Signup/Step2/MainContainer.js
+++ containers/Account/Signup/Step2/MainContainer.js
@@ -1,7 +1,7 @@
 import React from "react";
 import styled, { css } from "styled-components";
 import { inject, observer } from "mobx-react";
-
+import Button from "components/Button";
 import SelectCard from "./SelectCard";
 import Authentication from "./Authentication";
 import Explaination from "./Explaination";
@@ -9,13 +9,145 @@ import Product from "./Product";
 import Introduction from "./Introduction";
 import Portfolio from "./portfolio";
 import Location from "./Location";
-
+import * as Title from "components/Title";
+import * as Content from "components/Content";
 import * as Text from "components/Text";
+import { toJS } from "mobx";
 
-@inject("Auth", "Answer")
+const arrowRightImg = "/static/images/producer/arrow_right.svg";
+const checkImg = "/static/images/producer/check.svg";
+
+@inject("Category", "Partner")
 @observer
 class MainContainer extends React.Component {
+  async componentDidMount() {
+    const { Category } = this.props;
+    await Category.init();
+    console.log(Category.business_list)
+    var mainCategoryTypeDic = {};
+    var subCategoryTypeDic = {};
+    mainCategoryTypeDic["business"] = Category.mainbusiness_list;
+    mainCategoryTypeDic["category"] = Category.maincategory_list;
+    mainCategoryTypeDic["city"] = Category.city_list;
+    mainCategoryTypeDic["material"] = Category.mainmaterial_list;
+    mainCategoryTypeDic["develop"] = Category.developbig_list;
+
+    // console.log(Category.mainbusiness_list);
+
+    this.setState({ mainCategoryTypeDic: mainCategoryTypeDic });
+    console.log(this.state.mainCategoryTypeDic);
+  }
+  state = {
+    mainSelectIdx: 0,
+    subSelectIdx: 0,
+    mainCategoryTypeDic: {},
+
+    // 공정 대분류
+    develop_active: false,
+    // 소재 대분류
+    material_active: false,
+  };
+  activeHandler = (type, idx) => {
+    const { Category } = this.props;
+
+    if (type === "main") {
+      if (idx === this.state.mainSelectIdx) {
+        return true;
+      } else {
+        return false;
+      }
+    } else {
+      if (Category.category_selected.includes(idx)) {
+        return true;
+      } else {
+        return false;
+      }
+    }
+  };
+
+  buttonClick = (type, idx) => {
+    const { Category } = this.props;
+
+    // 공정 클릭 했을 때
+    if (type == "develop") {
+      if (Category.categoryActiveHandler(idx, type)) {
+        console.log("remove selected");
+        Category.remove_selected(type, idx);
+      } else {
+        console.log("add selected");
+        Category.add_selected(type, idx);
+      }
+    }
+    // 소재 클릭 했을 때
+    if (type == "material") {
+      if (Category.categoryActiveHandler(idx, type)) {
+        console.log("remove selected");
+        Category.remove_selected(type, idx);
+      } else {
+        console.log("add selected");
+        Category.add_selected(type, idx);
+      }
+    }
+
+    // 지역일 때는 다르게
+    if (this.props.type == "city") {
+      if (Category.categoryActiveHandler(idx, this.props.type)) {
+        console.log("remove selected");
+        Category.remove_selected(this.props.type, idx);
+      } else {
+        console.log("add selected");
+        Category.add_selected(this.props.type, idx);
+      }
+    }
+
+    if (type === "main") {
+      this.setState({ mainSelectIdx: idx });
+    } else {
+      if (Category.categoryActiveHandler(idx, this.props.type)) {
+        console.log("remove selected");
+        Category.remove_selected(this.props.type, idx);
+      } else {
+        console.log("add selected");
+        Category.add_selected(this.props.type, idx);
+      }
+      // this.setState({ subSelectIdx: idx });
+    }
+  };
+
+  // 소재, 공정 클릭 했을 때
+  CategoryClick = (state) => {
+    const { Category, type } = this.props;
+    if (state == "develop") {
+      //this.props.type = "develop"
+      this.setState({
+        ...this.state,
+        develop_active: !this.state.develop_active,
+        material_active: false,
+      });
+
+      console.log(this.state.develop_active);
+    }
+
+    if (state == "material") {
+      //this.props.type = "material"
+      this.setState({
+        ...this.state,
+        material_active: !this.state.material_active,
+        develop_active: false,
+      });
+    }
+  };
+
+  // 모달 종료하기
+  modalclose = () => {
+    const { Partner } = this.props;
+
+    Partner.filter_dropdown = false;
+  };
+
+
   render() {
+    const { Category, type } = this.props;
     return (
       <Container>
         <Name>(주)동성실리콘</Name>
@@ -23,10 +155,121 @@ class MainContainer extends React.Component {
           해당 정보를 채울수록 클라이언트에게 '동성 실리콘'(가)이 노출될 확률이
           올라가요!
         </Description>
-        <SelectCard name="전문분야" />
-        <SelectCard name="제품분야" />
-        <SelectCard name="취급 소재" />
-        <SelectCard name="전문 공정" />
+        <div style={{ width: "100%" }}>
+          <SubCategoryBox>
+            전문분야
+            <SubInnerBox>
+              {/* 카테고리 선택 */}
+              {Category.mainbusiness_list &&
+                Category.mainbusiness_list.map((data, idx) => {
+                  data.business_set.map((sub_data,idx) => {
+                    console.log(toJS(sub_data.category))
+                  return (
+                    <SubCategoryButton
+                      onClick={() => {
+                        this.buttonClick("sub", sub_data.id);
+                      }}
+                      active={Category.categoryActiveHandler(sub_data.id, type)}
+                    >
+                      <CheckBox active={Category.categoryActiveHandler(sub_data.id, type)}>
+                        <img src={checkImg} />
+                      </CheckBox>
+                      <SubCategoryFont>{sub_data.category}</SubCategoryFont>
+                    </SubCategoryButton>
+                  )  
+                }
+                  );
+                })}
+            </SubInnerBox>
+          </SubCategoryBox>
+        </div>
+
+        <div style={{ width: "100%" }}>
+          <SubCategoryBox>
+            제품분야
+            <SubInnerBox>
+              {/* 카테고리 선택 */}
+              {Category.maincategory_list &&
+                Category.maincategory_list.map((data, idx) => {
+                  data.category_set.map((sub_data,idx) => {
+                    console.log(toJS(sub_data.category))
+                  return (
+                    <SubCategoryButton
+                      onClick={() => {
+                        this.buttonClick("sub", sub_data.id);
+                      }}
+                      active={Category.categoryActiveHandler(sub_data.id, type)}
+                    >
+                      <CheckBox active={Category.categoryActiveHandler(sub_data.id, type)}>
+                        <img src={checkImg} />
+                      </CheckBox>
+                      <SubCategoryFont>{sub_data.category}</SubCategoryFont>
+                    </SubCategoryButton>
+                  )  
+                }
+                  );
+                })}
+            </SubInnerBox>
+          </SubCategoryBox>
+        </div>
+        
+        <div style={{ width: "100%" }}>
+          <SubCategoryBox>
+            취급소재
+            <SubInnerBox>
+              {/* 카테고리 선택 */}
+              {Category.mainmaterial_list &&
+                Category.mainmaterial_list.map((data, idx) => {
+                  data.material_set.map((sub_data,idx) => {
+                    console.log(toJS(sub_data.category))
+                  return (
+                    <SubCategoryButton
+                      onClick={() => {
+                        this.buttonClick("sub", sub_data.id);
+                      }}
+                      active={Category.categoryActiveHandler(sub_data.id, type)}
+                    >
+                      <CheckBox active={Category.categoryActiveHandler(sub_data.id, type)}>
+                        <img src={checkImg} />
+                      </CheckBox>
+                      <SubCategoryFont>{sub_data.category}</SubCategoryFont>
+                    </SubCategoryButton>
+                  )  
+                }
+                  );
+                })}
+            </SubInnerBox>
+          </SubCategoryBox>
+        </div>
+
+        <div style={{ width: "100%" }}>
+          <SubCategoryBox>
+            전문공정
+            <SubInnerBox>
+              {/* 카테고리 선택 */}
+              {Category.developbig_list &&
+                Category.developbig_list.map((data, idx) => {
+                  data.develop_set.map((sub_data,idx) => {
+                    console.log(toJS(sub_data.category))
+                  return (
+                    <SubCategoryButton
+                      onClick={() => {
+                        this.buttonClick("sub", sub_data.id);
+                      }}
+                      active={Category.categoryActiveHandler(sub_data.id, type)}
+                    >
+                      <CheckBox active={Category.categoryActiveHandler(sub_data.id, type)}>
+                        <img src={checkImg} />
+                      </CheckBox>
+                      <SubCategoryFont>{sub_data.category}</SubCategoryFont>
+                    </SubCategoryButton>
+                  )  
+                }
+                  );
+                })}
+            </SubInnerBox>
+          </SubCategoryBox>
+        </div>
         <Authentication></Authentication>
         <Explaination></Explaination>
         <Product></Product>
@@ -64,4 +307,91 @@ const Description = styled.div`
   font-weight: normal;
   margin-top: 30px;
   margin-bottom: 20px;
+`;
+
+const SubCategoryBox = styled.div`
+  height: 80%;
+  border-bottom: 1px solid #e1e2e4;
+  overflow-y: scroll;
+
+  @media (min-width: 0px) and (max-width: 767.98px) {
+    height: 85%;
+  }
+`;
+
+const SubInnerBox = styled.div`
+  padding: 34px 18px;
+  display: flex;
+  flex-wrap: wrap;
+
+  @media (min-width: 0px) and (max-width: 767.98px) {
+    padding: 20px 9px;
+  }
+  @media (min-width: 768px) and (max-width: 991.98px) {
+    padding: 27px 15px;
+  }
+`;
+const SubCategoryButton = styled.div`
+  width: 50%;
+  // background: ${(props) => (props.active ? "#ededef" : "none")};
+  background: none;
+  /* height:30px; */
+  display: flex;
+  align-items: center;
+  margin-bottom: 6px;
+`;
+const ButtonBox = styled.div`
+  display: flex;
+  justify-content: flex-end;
+  align-items: center;
+  height: 20%;
+  padding-right: 20px;
+
+  @media (min-width: 0px) and (max-width: 767.98px) {
+    height: 15%;
+  }
+`;
+const SubCategoryFont = styled(Content.FontSize14)`
+  font-weight: normal;
+  font-stretch: normal;
+  font-style: normal;
+  line-height: 2.43;
+  letter-spacing: -0.35px;
+  text-align: left;
+  color: #282c36;
+  cursor: pointer;
+  word-break: break-word;
+}`
+
+const CheckBox = styled.div`
+  width: 18px;
+  height: 18px;
+  border: 1px solid #c6c7cc;
+  margin-right: 12px;
+  cursor: pointer;
+  > img {
+    display: ${(props) => (props.active ? "block" : "none")};
+  }
+
+  @media (min-width: 0px) and (max-width: 767.98px) {
+    width: 14px;
+    height: 14px;
+    min-width: 14px;
+    min-height: 14px;
+    position: relative;
+    > img {
+      width: 15px;
+      height: 15px;
+      position: absolute;
+      top: -1px;
+    }
+  }
+  @media (min-width: 768px) and (max-width: 991.98px) {
+    width: 16px;
+    height: 16px;
+    > img {
+      width: 17px;
+      height: 17px;
+    }
+  }
 `;
