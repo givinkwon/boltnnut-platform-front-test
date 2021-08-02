e210d718ab6dec90ef8e34be38e436c5b0fd26b1
Nick <65061325+psw0962@users.noreply.github.com>
2021-08-02 09:38:04 +0900

    기능추가 : 제조사 찾기 페이지 이미지 검색 작업중

--- containers/Manufacture/Producer/FilterBoxSearchBar.js
+++ containers/Manufacture/Producer/FilterBoxSearchBar.js
@@ -6,21 +6,14 @@ import ButtonComponent from "components/Buttonv2";
 
 import Background from "components/Background";
 import Container from "components/Containerv1";
+import * as Title from "components/Title";
 import Router from "next/router";
 import { toJS } from "mobx";
 import AddFile from "./AddFile";
 import { PRIMARY2 } from "static/style";
 import Category from "../../../stores/Manufacture/Category";
 
-@inject(
-  "Auth",
-  "Project",
-  "Request",
-  "Partner",
-  "ManufactureProcess",
-  "Producer",
-  "Category"
-)
+@inject("Auth", "Project", "Request", "Partner", "ManufactureProcess", "Producer", "Category")
 @observer
 class SearchBarConatiner extends React.Component {
   state = {
@@ -31,7 +24,7 @@ class SearchBarConatiner extends React.Component {
     suggs: [],
     showSuggs: false,
     searchbaractive: false,
-
+    imgsearchhover: false,
   };
 
   // 검색함수
@@ -72,10 +65,7 @@ class SearchBarConatiner extends React.Component {
         // console.log(Partner.subButtonActive);
         ManufactureProcess.saveSearchText(Partner.search_text);
         ManufactureProcess.loadingSaveSearchText = false;
-        setTimeout(
-          () => (ManufactureProcess.loadingSaveSearchText = true),
-          2000
-        );
+        setTimeout(() => (ManufactureProcess.loadingSaveSearchText = true), 2000);
       }
     }
   };
@@ -104,11 +94,7 @@ class SearchBarConatiner extends React.Component {
     let y = $(document).scrollTop() + event.clientY; // event.offsetY
 
     // did not click on the search input or the suggestion list
-    if (
-      this.state.showSuggestions &&
-      !this.checkXYInElement(x, y, ".searcher-suggs") &&
-      !this.checkXYInElement(x, y, ".searcher-input")
-    ) {
+    if (this.state.showSuggestions && !this.checkXYInElement(x, y, ".searcher-suggs") && !this.checkXYInElement(x, y, ".searcher-input")) {
       this.setState({ showSuggestions: false });
     }
   }
@@ -126,12 +112,7 @@ class SearchBarConatiner extends React.Component {
       h: elem.outerHeight(),
     };
 
-    if (
-      x < rect.x ||
-      y < rect.y ||
-      x > rect.x + rect.w ||
-      y > rect.y + rect.h
-    ) {
+    if (x < rect.x || y < rect.y || x > rect.x + rect.w || y > rect.y + rect.h) {
       return false;
     }
 
@@ -142,9 +123,7 @@ class SearchBarConatiner extends React.Component {
   // true if the input was the same as the suggsKeywords
   // otherwise, false
   checkSuggsKeywords(keywords) {
-    if (
-      this.state.suggsKeywords == encodeURIComponent(keywords.toLowerCase())
-    ) {
+    if (this.state.suggsKeywords == encodeURIComponent(keywords.toLowerCase())) {
       return true;
     }
 
@@ -171,9 +150,7 @@ class SearchBarConatiner extends React.Component {
 
     let urlKeywords = encodeURIComponent(keywords.toLowerCase());
     this.setState({ suggsKeywords: urlKeywords, suggs: [] });
-    let url =
-      "https://suggestqueries.google.com/complete/search?output=chrome&q=" +
-      urlKeywords;
+    let url = "https://suggestqueries.google.com/complete/search?output=chrome&q=" + urlKeywords;
     // use JSONP (issue: http://security.stackexchange.com/questions/23438/security-risks-with-jsonp/23439#23439)
     // just for CORS trick
     $.ajax({
@@ -272,7 +249,16 @@ class SearchBarConatiner extends React.Component {
   imageModal = () => {
     const { Partner } = this.props;
     Partner.image_modal_state = !Partner.image_modal_state;
-  }
+  };
+
+  // 이미지 버큰 호버 시 문구 안내 핸들러 함수
+  imageSearchHandler = () => {
+    if (!this.state.imgsearchhover) {
+      this.setState({ ...this.state, imgsearchhover: true });
+    } else {
+      this.setState({ ...this.state, imgsearchhover: false });
+    }
+  };
 
   render() {
     const { Partner, Request } = this.props;
@@ -280,19 +266,11 @@ class SearchBarConatiner extends React.Component {
     let suggestions = null;
     // Partner.searchText가 처음에 null 값이라 에러가 떠서 공백문자를 더해줌
     // 구글 검색 제안 리스트
-    if (
-      this.state.showSuggestions &&
-      this.checkSuggsKeywords(Partner.search_text + "")
-    ) {
+    if (this.state.showSuggestions && this.checkSuggsKeywords(Partner.search_text + "")) {
       suggestions = this.state.suggs.map(
         function (value, index) {
           return (
-            <li
-              key={index}
-              className="searcher-suggs-word"
-              onClick={this.handleClickSuggetionsKeywords.bind(this)}
-              onMouseOver={this.handleHoverSearcherSuggestions.bind(this)}
-            >
+            <li key={index} className="searcher-suggs-word" onClick={this.handleClickSuggetionsKeywords.bind(this)} onMouseOver={this.handleHoverSearcherSuggestions.bind(this)}>
               {value}
             </li>
           );
@@ -311,34 +289,41 @@ class SearchBarConatiner extends React.Component {
               alignItems: "center",
             }}
           >
-            <SearchBar active={Partner.subButtonActive}>
+            <SearchBar active={Partner.subButtonActive} style={{ position: "relative" }}>
               <input
                 placeholder="원하는 분야의 제조업체나 비슷한 제품을 검색해보세요."
                 onFocus={(e) => (e.target.placeholder = "")}
-                onBlur={(e) =>
-                  (e.target.placeholder =
-                    "원하는 분야의 제조업체나 비슷한 제품을 검색해보세요.")
-                }
+                onBlur={(e) => (e.target.placeholder = "원하는 분야의 제조업체나 비슷한 제품을 검색해보세요.")}
                 onChange={this.handleSearcherInputChange.bind(this)}
                 value={Partner.search_text}
                 class="Input"
                 onKeyPress={this.handleKeyDown}
               />
-              <img src="/static/icon/Camera.svg" onClick={this.imageModal} />
+
+              <HoverBox active={this.state.imgsearchhover}>
+                <Title13>제품 이미지로 검색하기</Title13>
+              </HoverBox>
+
+              <ImgContainer onMouseEnter={() => this.imageSearchHandler()} onMouseLeave={() => this.imageSearchHandler()}>
+                <ImgBox src="/static/icon/camera.svg" onClick={this.imageModal} />
+              </ImgContainer>
+
               {/* 이미지 검색 테스트용 */}
-              {Partner.image_modal_state &&
+              {Partner.image_modal_state && (
                 <span style={{ display: "inline-block" }}>
-                <AddFile
-                  file={true}
-                  isOpen={true}
-                  ///onChange={this.handleChange}
-                />
-                <div></div>
-              </span>
-              }
-              <img src="/static/icon/search_blue.svg" onClick={this.search} />
+                  <AddFile
+                    file={true}
+                    isOpen={true}
+                    ///onChange={this.handleChange}
+                  />
+                  <div></div>
+                </span>
+              )}
+              <ImgContainer>
+                <img src="/static/icon/search_blue.svg" onClick={this.search} />
+              </ImgContainer>
             </SearchBar>
-            
+
             {this.state.showSuggestions && this.state.suggs.length > 0 && (
               <CustomUl>
                 <CustomLiBox>{suggestions}</CustomLiBox>
@@ -352,6 +337,49 @@ class SearchBarConatiner extends React.Component {
 }
 
 export default SearchBarConatiner;
+
+const ImgContainer = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+  width: 45px;
+  height: 45px;
+  border-radius: 20px;
+
+  :hover {
+    background-color: #eeeeee;
+  }
+`;
+
+const HoverBox = styled.div`
+  display: ${(props) => (props.active ? "flex" : "none")};
+  justify-content: center;
+  align-items: center;
+  width: 190px;
+  height: 49px;
+  border-radius: 75px;
+  box-shadow: 0 3px 6px 0 rgba(0, 0, 0, 0.16);
+  background-color: #ffffff;
+  margin-bottom: 5px;
+
+  position: absolute;
+  bottom: 100%;
+  left: 75%;
+`;
+
+const Title13 = styled(Title.FontSize13)`
+  font-weight: normal;
+  font-stretch: normal;
+  line-height: 4;
+  letter-spacing: -0.33px;
+  color: #0933b3;
+`;
+
+const ImgBox = styled.img`
+  width: 32px;
+  height: 32px;
+`;
 
 const CustomUl = styled.ul`
   width: 100%;
@@ -398,7 +426,7 @@ const SearchBar = styled.div`
   padding-right: 25px;
 
   input {
-    width: 700px;
+    width: 640px;
     height: 59px;
     border: none;
     border-radius: 60px;
