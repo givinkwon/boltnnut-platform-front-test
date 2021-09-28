2486cc60db7feec503d8fd91763fe23fd3ce21e9
givinkwon <rlqls505@naver.com>
2021-09-28 14:10:45 +0900

    오류 수정 : 캘린더 오류 수정

--- stores/Manufacture/Schedule.js
+++ stores/Manufacture/Schedule.js
@@ -13,7 +13,7 @@ class Schedule {
   @observable current = "10:00:00"; // 현재시간
   @observable inactive_today = []; // 선택된 날짜 + current ~ 선택된 날짜 19:00:00.00 까지 안 되는 시간들
   @observable book_time = null; // today + current (2021-11-11 10:00:00)
-  @observable nowMoment = moment();
+  @observable nowMoment = moment(); // 현재 날짜 가져오기
   @observable calendarOnOff = true;
   @observable calendarOnOffV2 = false;
   @observable clickDay = 0;
@@ -42,12 +42,14 @@ class Schedule {
     this.getDays(today_date.split("-")[0], today_date.split("-")[1]);
   };
 
+  // 현재날짜라 클릭된 것으로 선택
   @action setTodayDate = (obj) => {
     this.today = obj;
     this.current = "10:00:00";
     this.book_time = this.today + this.current;
 
     this.getDays(this.today.split("-")[0], this.today.split("-")[1]);
+    // 이미 예약되어 있는 날짜를 DB에서 가져와서 비활성화
     this.getOccupiedDate();
   };
 
@@ -57,6 +59,7 @@ class Schedule {
     console.log(this.book_time);
   };
 
+  // 이미 예약되어 있는 날짜를 DB에서 가져와서 비활성화하는 함수
   @action getOccupiedDate = () => {
     this.inactive_today = [];
     let req = {
@@ -102,6 +105,7 @@ class Schedule {
 
   @action fullDateCheck = (startAt, endAt) => {
     console.log("fullDateCheck돌립니다.");
+    console.log(startAt, endAt)
     let req = {
       startAt: startAt,
       endAt: endAt,
