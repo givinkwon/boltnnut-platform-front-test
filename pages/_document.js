26fd89140de6a13a3e3efd160174e79b265511ce
wisysta <choi199042@gmail.com>
2022-07-28 15:04:13 +0900

    기본 SEO 오류 수정

--- pages/_document.js
+++ pages/_document.js
@@ -3,35 +3,38 @@ import { ServerStyleSheet as StyledComponentSheets } from "styled-components";
 import { ServerStyleSheets as MaterialUiServerStyleSheets } from "@material-ui/styles";
 
 export default class MyDocument extends Document {
-  static getInitialProps = async (ctx) => {
-    const styledComponentSheet = new StyledComponentSheets();
-    const materialUiSheets = new MaterialUiServerStyleSheets();
-    const originalRenderPage = ctx.renderPage;
+    static getInitialProps = async (ctx) => {
+        const styledComponentSheet = new StyledComponentSheets();
+        const materialUiSheets = new MaterialUiServerStyleSheets();
+        const originalRenderPage = ctx.renderPage;
 
-    try {
-      ctx.renderPage = () =>
-        originalRenderPage({
-          enhanceApp: (App) => (props) => styledComponentSheet.collectStyles(materialUiSheets.collect(<App {...props} />)),
-        });
-      const initialProps = await Document.getInitialProps(ctx);
-      return {
-        ...initialProps,
-        styles: [
-          <React.Fragment key="styles">
-            {initialProps.styles}
-            {materialUiSheets.getStyleElement()}
-            {styledComponentSheet.getStyleElement()}
-          </React.Fragment>,
-        ],
-      };
-    } finally {
-      styledComponentSheet.seal();
-    }
-  };
+        try {
+            ctx.renderPage = () =>
+                originalRenderPage({
+                    enhanceApp: (App) => (props) =>
+                        styledComponentSheet.collectStyles(
+                            materialUiSheets.collect(<App {...props} />)
+                        ),
+                });
+            const initialProps = await Document.getInitialProps(ctx);
+            return {
+                ...initialProps,
+                styles: [
+                    <React.Fragment key="styles">
+                        {initialProps.styles}
+                        {materialUiSheets.getStyleElement()}
+                        {styledComponentSheet.getStyleElement()}
+                    </React.Fragment>,
+                ],
+            };
+        } finally {
+            styledComponentSheet.seal();
+        }
+    };
 
-  setReportV4() {
-    return {
-      __html: `
+    setReportV4() {
+        return {
+            __html: `
       var VIEW_ID = '214568260';
 
       // Query the API and print the results to the page.
@@ -104,11 +107,11 @@ export default class MyDocument extends Document {
       }
 
      `,
-    };
-  }
-  setAnalyticsApi() {
-    return {
-      __html: `
+        };
+    }
+    setAnalyticsApi() {
+        return {
+            __html: `
       console.log(1)
       // Replace with your client ID from the developer console.
       var CLIENT_ID ='392846125574-q1os3ihbrss3u4hj7gcvkjhk6at6g7dl.apps.googleusercontent.com'; // <-- 발급받은 Client ID 입력 
@@ -257,11 +260,11 @@ export default class MyDocument extends Document {
       // a();
       // alert("F");
       `,
-    };
-  }
-  setGoogleTags() {
-    return {
-      __html: `
+        };
+    }
+    setGoogleTags() {
+        return {
+            __html: `
       
       window.dataLayer = window.dataLayer || [];
       
@@ -280,11 +283,11 @@ export default class MyDocument extends Document {
       }
       
      `,
-    };
-  }
-  setChannelTalk() {
-    return {
-      __html: `
+        };
+    }
+    setChannelTalk() {
+        return {
+            __html: `
         (function() {
           var w = window;
           if (w.ChannelIO) {
@@ -325,12 +328,12 @@ export default class MyDocument extends Document {
           "pluginKey": "ec3d75f3-0b9e-4b01-a746-dfed77e31339"
         });
       `,
-    };
-  }
+        };
+    }
 
-  setBeusable() {
-    return {
-      __html: `
+    setBeusable() {
+        return {
+            __html: `
         (function() {
           var w = window;
           var d = document;
@@ -345,21 +348,21 @@ export default class MyDocument extends Document {
           w.__beusablerumclient__.load(a);
       })();
       `,
-    };
-  }
+        };
+    }
 
-  setKakaoLogin() {
-    return {
-      __html: `
+    setKakaoLogin() {
+        return {
+            __html: `
         (function() {
           Kakao.init("1469e9509222cfd066d35737d4359063");
       })();
       `,
-    };
-  }
-  setBeusableAddition() {
-    return {
-      __html: `
+        };
+    }
+    setBeusableAddition() {
+        return {
+            __html: `
         (function(w, d, a){
           w.__baclient__ = {
               load : function(src){
@@ -370,54 +373,94 @@ export default class MyDocument extends Document {
           };w.__baclient__.load(a);
       })(window, document, "//ba.beusable.net/script/ba/c08687bf6b");
       `,
-    };
-  }
-  render() {
-    return (
-      <html>
-        <Head>
-          <>
-            {/* 페이스북 설치 태그*/}
-            <meta name="facebook-domain-verification" content="l31dir7jjxp2c050hcf9cjdnjpgi50" />
-            {/* 구글 옵티마이저 설치태그 */}
-            <script src="https://www.googleoptimize.com/optimize.js?id=OPT-56ZDC8G"></script>
-            <script data-ad-client="ca-pub-6441704928060549" async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+        };
+    }
+    render() {
+        return (
+            <html>
+                <Head>
+                    <>
+                        {/* 페이스북 설치 태그*/}
+                        <meta
+                            name="facebook-domain-verification"
+                            content="l31dir7jjxp2c050hcf9cjdnjpgi50"
+                        />
+                        {/* 구글 옵티마이저 설치태그 */}
+                        {/* <script src="https://www.googleoptimize.com/optimize.js?id=OPT-56ZDC8G"></script>
+                        <script
+                            data-ad-client="ca-pub-6441704928060549"
+                            async
+                            src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"
+                        ></script> */}
 
-            {/* Beusable */}
-            <script type="text/javascript"></script>
-            <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-            <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-            {/*--PG크로스브라우징필수내용--*/}
-            <meta http-equiv="Cache-Control" content="no-cache" />
-            <meta http-equiv="Expires" content="0" />
-            <meta http-equiv="Pragma" content="no-cache" />
+                        {/* Beusable */}
+                        <script type="text/javascript"></script>
+                        <meta
+                            http-equiv="Content-Type"
+                            content="text/html; charset=utf-8"
+                        />
+                        <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+                        {/*--PG크로스브라우징필수내용--*/}
+                        <meta http-equiv="Cache-Control" content="no-cache" />
+                        <meta http-equiv="Expires" content="0" />
+                        <meta http-equiv="Pragma" content="no-cache" />
 
-            {/* 네이버웹마스터도구 사이트구조개선 코드*/}
-            <meta name="NaverBot" content="All" />
-            <meta name="NaverBot" content="index, follow" />
-            <meta name="Yeti" content="All" />
-            <meta name="Yeti" content="index, follow" />
+                        {/* 네이버웹마스터도구 사이트구조개선 코드*/}
+                        <meta name="NaverBot" content="All" />
+                        <meta name="NaverBot" content="index, follow" />
+                        <meta name="Yeti" content="All" />
+                        <meta name="Yeti" content="index, follow" />
 
-            <meta name="google-signin-client_id" content="392846125574-q1os3ihbrss3u4hj7gcvkjhk6at6g7dl.apps.googleusercontent.com"></meta>
-            <meta name="google-signin-scope" content="https://www.googleapis.com/auth/analytics.readonly"></meta>
-            {/* 대표 URL */}
-            <link rel="canonical" href="https://www.boltnnut.com/" />
-            {/* Naver webmaster */}
-            <meta name="naver-site-verification" content="4354dc20bc3fb28a16e1db9800acf406fd782d76" />
-            {/* favicon */}
-            <link rel="shortcut icon" href="C:\Users\user\boltnnut-platform-front\public\favicon.ico" />
-            <link rel="shortcut icon" href="https://www.boltnnut.com/favicon.ico" />
-            <link rel="shortcut icon" href="/public/favicon.ico" />
-            <link rel="icon" href="C:\Users\user\boltnnut-platform-front\public\favicon.ico" />
-            <link rel="icon" href="/public/favicon.ico" />
-            <link href="https://fonts.googleapis.com/css?family=Noto+Sans+KR:100,300,400,500,700,900|Noto+Sans:400,700&display=swap" rel="stylesheet" />
-            <link rel="stylesheet" type="text/css" charset="UTF-8" href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick.min.css" />
-            <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick-theme.min.css" />
-          </>
-          {/* Google Tag Manager */}
-          <script
-            dangerouslySetInnerHTML={{
-              __html: `
+                        {/* <meta
+                            name="google-signin-client_id"
+                            content="392846125574-q1os3ihbrss3u4hj7gcvkjhk6at6g7dl.apps.googleusercontent.com"
+                        ></meta>
+                        <meta
+                            name="google-signin-scope"
+                            content="https://www.googleapis.com/auth/analytics.readonly"
+                        ></meta> */}
+                        {/* 대표 URL */}
+                        <link rel="canonical" href="https://www.k-jangin.com" />
+                        {/* Naver webmaster */}
+                        {/* <meta
+                            name="naver-site-verification"
+                            content="4354dc20bc3fb28a16e1db9800acf406fd782d76"
+                        /> */}
+                        {/* favicon */}
+                        <link
+                            rel="shortcut icon"
+                            href="C:\Users\user\boltnnut-platform-front\public\favicon.ico"
+                        />
+                        <link
+                            rel="shortcut icon"
+                            href="https://www.k-jangin.com/favicon.ico"
+                        />
+                        <link rel="shortcut icon" href="/public/favicon.ico" />
+                        <link
+                            rel="icon"
+                            href="C:\Users\user\boltnnut-platform-front\public\favicon.ico"
+                        />
+                        <link rel="icon" href="/public/favicon.ico" />
+                        <link
+                            href="https://fonts.googleapis.com/css?family=Noto+Sans+KR:100,300,400,500,700,900|Noto+Sans:400,700&display=swap"
+                            rel="stylesheet"
+                        />
+                        <link
+                            rel="stylesheet"
+                            type="text/css"
+                            charset="UTF-8"
+                            href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick.min.css"
+                        />
+                        <link
+                            rel="stylesheet"
+                            type="text/css"
+                            href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick-theme.min.css"
+                        />
+                    </>
+                    {/* Google Tag Manager */}
+                    <script
+                        dangerouslySetInnerHTML={{
+                            __html: `
               (function(w,d,s,l,i){
                 w[l]=w[l]||[];w[l].push({
                   'gtm.start':new Date().getTime(),event:'gtm.js'
@@ -426,53 +469,86 @@ export default class MyDocument extends Document {
                 var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
                 
               })(window,document,'script','dataLayer','GTM-TSW6VSN');`,
-            }}
-            async
-          ></script>
-        </Head>
-        <body oncontextmenu="return false" ondragstart="return false" onselectstart="return false">
-          <noscript>
-            <iframe
-              src="https://www.googletagmanager.com/ns.html?id=GTM-TSW6VSN"
-              height="0"
-              width="0"
-              style={{ display: "none", visibility: "hidden" }}
-            ></iframe>
-          </noscript>
-          <Main />
-          <script dangerouslySetInnerHTML={this.setChannelTalk()} />
-          <script dangerouslySetInnerHTML={this.setBeusable()} />
-          <script dangerouslySetInnerHTML={this.setBeusableAddition()} />
-          <NextScript />
-          {/* GA Settings*/}
-          <script async src="https://www.googletagmanager.com/gtag/js?id=UA-162026812-1"></script>
-          <script dangerouslySetInnerHTML={this.setGoogleTags()} />
+                        }}
+                        async
+                    ></script>
+                </Head>
+                <body
+                    oncontextmenu="return false"
+                    ondragstart="return false"
+                    onselectstart="return false"
+                >
+                    <noscript>
+                        <iframe
+                            src="https://www.googletagmanager.com/ns.html?id=GTM-TSW6VSN"
+                            height="0"
+                            width="0"
+                            style={{ display: "none", visibility: "hidden" }}
+                        ></iframe>
+                    </noscript>
+                    <Main />
+                    <script dangerouslySetInnerHTML={this.setChannelTalk()} />
+                    <script dangerouslySetInnerHTML={this.setBeusable()} />
+                    <script
+                        dangerouslySetInnerHTML={this.setBeusableAddition()}
+                    />
+                    <NextScript />
+                    {/* GA Settings*/}
+                    {/* <script
+                        async
+                        src="https://www.googletagmanager.com/gtag/js?id=UA-162026812-1"
+                    ></script>
+                    <script dangerouslySetInnerHTML={this.setGoogleTags()} /> */}
 
-          {/* Iamport */}
-          <script type="text/javascript" src="https://code.jquery.com/jquery-1.12.4.min.js" async></script>
-          <script type="text/javascript" src="https://cdn.iamport.kr/js/iamport.payment-1.1.5.js" async></script>
-          {/* Naver analic */}
-          <script type="text/javascript" src="//wcs.naver.net/wcslog.js"></script>
-          {/* Google Adsense */}
-          <script data-ad-client="ca-pub-6441704928060549" async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+                    {/* Iamport */}
+                    {/* <script
+                        type="text/javascript"
+                        src="https://code.jquery.com/jquery-1.12.4.min.js"
+                        async
+                    ></script>
+                    <script
+                        type="text/javascript"
+                        src="https://cdn.iamport.kr/js/iamport.payment-1.1.5.js"
+                        async
+                    ></script> */}
+                    {/* Naver analic */}
+                    {/* <script
+                        type="text/javascript"
+                        src="//wcs.naver.net/wcslog.js"
+                    ></script> */}
+                    {/* Google Adsense */}
+                    {/* <script
+                        data-ad-client="ca-pub-6441704928060549"
+                        async
+                        src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"
+                    ></script> */}
 
-          {/* 카카오로그인 */}
-          <script type="text/javascript" src="https://developers.kakao.com/sdk/js/kakao.js"></script>
-          <script dangerouslySetInnerHTML={this.setKakaoLogin()} />
-          {/* 테스트 */}
+                    {/* 카카오로그인 */}
+                    <script
+                        type="text/javascript"
+                        src="https://developers.kakao.com/sdk/js/kakao.js"
+                    ></script>
+                    <script dangerouslySetInnerHTML={this.setKakaoLogin()} />
+                    {/* 테스트 */}
 
-          <script type="text/javascript" src="https://apis.google.com/js/client.js?onload=authorize"></script>
-          <script type="text/javascript" src="https://apis.google.com/js/client:platform.js"></script>
-          <script dangerouslySetInnerHTML={this.setReportV4()} />
+                    <script
+                        type="text/javascript"
+                        src="https://apis.google.com/js/client.js?onload=authorize"
+                    ></script>
+                    <script
+                        type="text/javascript"
+                        src="https://apis.google.com/js/client:platform.js"
+                    ></script>
+                    <script dangerouslySetInnerHTML={this.setReportV4()} />
 
-          {/* <button id="auth-button" onclick="authorize()">
+                    {/* <button id="auth-button" onclick="authorize()">
             Authorize
           </button> */}
-          {/* <textarea cols="80" rows="20" id="query-output"></textarea> */}
-          {/* ㅌㅌㅌ */}
-          {/* <p class="g-signin2" data-onsuccess="batchGet"></p> */}
-        </body>
-      </html>
-    );
-  }
+                    {/* <textarea cols="80" rows="20" id="query-output"></textarea> */}
+                    {/* ㅌㅌㅌ */}
+                    {/* <p class="g-signin2" data-onsuccess="batchGet"></p> */}
+                </body>
+            </html>
+        );
+    }
 }
--- pages/index.js
+++ pages/index.js
@@ -1,27 +1,27 @@
-import React from 'react';
-import Head from 'next/head';
-import Router from 'next/router';
-import styled from 'styled-components';
-import { inject, observer } from 'mobx-react';
+import React from "react";
+import Head from "next/head";
+import Router from "next/router";
+import styled from "styled-components";
+import { inject, observer } from "mobx-react";
 
-import Nav from 'components/Nav';
-import MobileNav from 'components/MobileNav';
-import Footer from 'components/Footer';
-import Spinner from 'components/Spinner';
+import Nav from "components/Nav";
+import MobileNav from "components/MobileNav";
+import Footer from "components/Footer";
+import Spinner from "components/Spinner";
 // test
-import HomeConatiner from 'containers/Home';
-import 'slick-carousel/slick/slick.css';
-import 'slick-carousel/slick/slick-theme.css';
-import { toJS } from 'mobx';
+import HomeConatiner from "containers/Home";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import { toJS } from "mobx";
 
-const logo_ic = '/static/images/components/MobileNav/MobileLogo.svg';
-const AD = '/static/images/Home/AD.svg';
-const ADback = '/static/images/Home/ADback.png';
-const ADbackground = '/static/images/Home/ADbackground.svg';
+const logo_ic = "/static/images/components/MobileNav/MobileLogo.svg";
+const AD = "/static/images/Home/AD.svg";
+const ADback = "/static/images/Home/ADback.png";
+const ADbackground = "/static/images/Home/ADbackground.svg";
 
-import * as AccountAPI from 'axios/Account/Account';
+import * as AccountAPI from "axios/Account/Account";
 
-@inject('Home', 'Loading', 'Auth', 'Category')
+@inject("Home", "Loading", "Auth", "Category")
 @observer
 class Home extends React.Component {
     state = {
@@ -52,12 +52,12 @@ class Home extends React.Component {
         this.props.Loading.setOpen(true);
 
         this.props.Auth.home_index = 0;
-        this.props.Auth.previous_url = '';
+        this.props.Auth.previous_url = "";
 
         // 새로고침 방지
-        document.addEventListener('keydown', this._handleKeyDown);
+        document.addEventListener("keydown", this._handleKeyDown);
 
-        window.addEventListener('resize', this.updateDimensions);
+        window.addEventListener("resize", this.updateDimensions);
         this.setState({ ...this.state, width: window.innerWidth });
         setTimeout(() => {
             this.props.Loading.setOpen(false);
@@ -73,14 +73,17 @@ class Home extends React.Component {
         // document.referrer은 next.js 페이지 내부에서의 이동이 안잡힘
         // 페이지 내에 이동이 있는 경우 => 신규가 아님
         if (history.length > 1) {
-            formData.append('prevUrl', window.location.href + history[history.length - 2]);
+            formData.append(
+                "prevUrl",
+                window.location.href + history[history.length - 2]
+            );
         } else {
-            document.referrer === ''
-                ? formData.append('prevUrl', 'direct')
-                : formData.append('prevUrl', document.referrer);
+            document.referrer === ""
+                ? formData.append("prevUrl", "direct")
+                : formData.append("prevUrl", document.referrer);
         }
 
-        formData.append('url', window.location.href);
+        formData.append("url", window.location.href);
         const req = {
             data: formData,
         };
@@ -106,7 +109,7 @@ class Home extends React.Component {
             });
     }
     componentWillUnmount() {
-        window.removeEventListener('resize', this.updateDimensions);
+        window.removeEventListener("resize", this.updateDimensions);
     }
     updateDimensions = () => {
         this.setState({ ...this.state, width: window.innerWidth });
@@ -122,21 +125,30 @@ class Home extends React.Component {
                     {/* SEO */}
                     <meta
                         name="description"
-                        content="제품군별 제조 전문가 큐레이션 플랫폼 볼트앤너트. 믿음직한 제품 개발업체를 만나는 가장 쉬운 방법! 시제품부터 생활용품까지 모두 OK!"
+                        content="CNC 가공 빠르고, 안전하게 AI 견적으로 제조 부품 바로발주 3D 도면만 올리면 부품 납기, 견적이 바로! 즉시 견적 확인하고 편하게 발주하세요."
                     />
                     <meta
                         name="keywords"
-                        content="제조, 제조업, 제조업체, 제조회사, 제품개발, 외주용역, 제조업체찾기, 제품제작, ODM, 제품제조, OEM, 개발구매, 부품구매, 구매부서, 시제품제작, 금형, 설계, 기구설계, 회로설계, 양산, 생산, 공장, MOQ, 발주, 의뢰, 납기, 품질, 견적, 공장찾기, 제조공장, 입찰, 금속가공, 실리콘, 사출, 플라스틱, 다이캐스팅, 절곡, 프레스, PCB, SMT, 연구개발, 산업장비 및 부품, 가공, 목업, Mock-up, 자동차용품, 가전/디지털, 기계설계, 설비, 전략구매, 도면, 캐드"
+                        content="제조, 제조업, 제조업체, 제조회사, 제품개발, 외주용역, 제조업체찾기, 제품제작,노즐, 스텐, 스뎅, 크랭크축, CNC, 알루미늄CNC, 금속CNC, SUS304, 서스, 부품가공, 기계부품 ODM, 제품제조, OEM, 개발구매, 부품구매, 구매부서, 시제품제작, 금형, 설계, 기구설계, 회로설계, 양산, 생산, 공장, MOQ, 발주, 의뢰, 납기, 품질, 견적, 공장찾기, 제조공장, 입찰, 금속가공, 실리콘, 사출, 플라스틱, 다이캐스팅, 절곡, 프레스, PCB, SMT, 연구개발, 산업장비 및 부품, 가공, 목업, Mock-up, 자동차용품, 가전/디지털, 기계설계, 설비, 전략구매, 도면, 캐드"
                     />
                     {/* SEO - open graph*/}
                     <meta property="og:type" content="website" />
-                    <meta property="og:image" content="/static/images/thumbnail.png" />
-                    <meta property="og:title" content="볼트앤너트|믿을 수 있는 제조 전문가" />
+                    <meta
+                        property="og:image"
+                        content="/static/images/thumbnail.png"
+                    />
+                    <meta
+                        property="og:title"
+                        content="바로발주 | AI 견적으로 제조부품 발주"
+                    />
                     <meta
                         property="og:description"
-                        content="제품군별 제조 전문가 큐레이션 플랫폼 볼트앤너트. 믿음직한 제품 개발업체를 만나는 가장 쉬운 방법! 시제품부터 생활용품까지 모두 OK!"
+                        content=" AI 견적으로 제조 부품 바로발주 3D 도면만 올리면 부품 납기, 견적이 바로! 즉시 견적 확인하고 편하게 발주하세요."
                     />
-                    <meta property="og:url" content="https://www.boltnnut.com/" />
+                    <meta
+                        property="og:url"
+                        content="https://www.k-jangin.com"
+                    />
                     {/* Title */}
                     <title>볼트앤너트</title>
                 </Head>
@@ -145,7 +157,9 @@ class Home extends React.Component {
                     {Loading.is_open}
 
                     <>
-                        {width && width < 768 && <MobileNav src={logo_ic} width={width} />}
+                        {width && width < 768 && (
+                            <MobileNav src={logo_ic} width={width} />
+                        )}
                         {width && width > 767.98 && <Nav width={width} />}
                     </>
                     {/* {width && width > 768 && (
@@ -157,7 +171,10 @@ class Home extends React.Component {
           )} */}
                     <>
                         {width && (
-                            <HomeConatiner width={width} reqList={Home.request_list} /> // 볼트앤너트 메인 페이지
+                            <HomeConatiner
+                                width={width}
+                                reqList={Home.request_list}
+                            /> // 볼트앤너트 메인 페이지
                         )}
                     </>
                     <>{width && <Footer BackgroundColor="#fff" />}</>
